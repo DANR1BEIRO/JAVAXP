003e919ebadb304404066abb666a41c0bada0bf7
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-28 10:17:41 -0300

    Diretório corrigido

--- DIO/src/smartTV/SmartTV.java
+++ DIO/src/smartTV/SmartTV.java
@@ -17,6 +17,7 @@ public class SmartTV {
     }
 
     public void increaseVolume(){
+
         volume++;
     }
 
@@ -25,10 +26,12 @@ public class SmartTV {
     }
 
     public boolean getOn() {
+
         return on;
     }
 
     public void setOn(boolean on) {
+
         this.on = on;
     }
 
@@ -37,30 +40,37 @@ public class SmartTV {
     }
 
     public void setChannel_max(int channel_max) {
+
         this.channel_max = channel_max;
     }
 
     public int getChannel_min() {
+
         return channel_min;
     }
 
     public void setChannel_min(int channel_min) {
+
         this.channel_min = channel_min;
     }
 
     public int getChannel() {
+
         return channel;
     }
 
     public void setChannel(int channel) {
+
         this.channel = channel;
     }
 
     public int getVolume() {
+
         return volume;
     }
 
     public void setVolume(int volume) {
+
         this.volume = volume;
     }
    
