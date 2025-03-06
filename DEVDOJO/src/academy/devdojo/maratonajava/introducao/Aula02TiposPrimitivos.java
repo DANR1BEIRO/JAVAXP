a514b7cc28a5d8ecbf0f14037edcc914fefd891e
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-06 02:52:14 -0300

    Primitive types

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        byte byteNumber = 127; // -128 to 127
+        short shortNumber = 32767; // - 32768 to 32767
+        int intNumber = 2147483647; // - 2147483648 to 2147483647
+        long longNumber = 1000000000;
+        float floatNumber = 2500.0f;
+        double doubleNumber = 2500.00;
+        boolean bool = false;
+        char character = 'a';
+        String name = "Goku";
+
+        System.out.println(name);
+
+
+
+
+
+    }
+}
---
+++ DEVDOJO/src/academy/devdojo/maratonajava/introducao/Aula3TiposPrimitivosExercicio.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula3TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+
+        String name = "Goku";
+        String address = "Kaioh planet";
+        int salary = 1000000000;
+        String date = "on march 5, 2025.";
+        String result = ("I, " + name + ", living at the address " + address + ", confirm that i receiverd the salary " +
+                "of DB$" + salary + date);
+
+        System.out.println(result);
+    }
+}
