099076aa7f28b316bfb917d6d21e6481dc87ff62
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-07 02:48:06 -0300

    Hellow world dojo

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/introducao/Class01HelloDevDojo.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Class01HelloDevDojo {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Devdojo!");
+    }
+}
