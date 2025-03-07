4ffbf24630ecec386a8865bfb09801d27f31e566
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-07 02:44:44 -0300

    Operators

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/introducao/Class04Operators.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Class04Operators {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 20;
+        int sum = number1 + number2;
+        int product = number1 * number2;
+        int subtraction = number1 - number2;
+        int division = number1 / number2;
+
+        System.out.println("Sum: " + (number2 + number1) +
+                           "\nProduct: " + product +
+                           "\nSubtraction: " + subtraction +
+                           "\nDivision: " + division);
+    }
+}
