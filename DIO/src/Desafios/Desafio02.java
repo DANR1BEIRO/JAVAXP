fb84af702770c755e7a26c80ba06cd7516ae3c24
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-08 20:14:48 -0300

    Classifica os clientes com base no saldo

---
+++ DIO/src/Desafios/Desafio02.java
@@ -0,0 +1,28 @@
+package Desafios;
+/*
+* Crie um programa que classifique os clientes de um banco com base no saldo de suas contas.
+*  O programa deve receber o saldo e classificá-lo em três categorias:
+    "Negativado" para saldos abaixo de zero.
+    "Baixo" para saldos entre 0 e 500, inclusive.
+    "Confortavel" para saldos acima de 500.
+*/
+
+import java.util.Scanner;
+
+public class Desafio02 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double saldo = scanner.nextDouble();
+
+        if (saldo < 0) {
+            System.out.println("Negativado");
+        } else if(saldo >= 0 && saldo <= 500) {
+            System.out.println("Baixo");
+        } else {
+            System.out.println("Confortavel");
+        }
+        scanner.close();
+    }
+}
