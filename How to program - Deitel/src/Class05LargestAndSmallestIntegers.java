b19cd8c973c55b862ab9ce2c4d9cafbb932797d4
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-09 01:51:43 -0300

    Receive five inputs and returns the largest and smallest of them

---
+++ How to program - Deitel/src/Class05LargestAndSmallestIntegers.java
@@ -0,0 +1,30 @@
+/*
+ * Write an application that reads five integers and determines and
+ * prints the largest and smallest integers in the group.
+ */
+
+import java.util.Scanner;
+public class Class05LargestAndSmallestIntegers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print("Enter integer " + ( i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+        int largest = numbers[0],
+            smallest = numbers[0];
+
+        for (int i = 1; i < 5; i ++ ) {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            } else {
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("The largest integer is: " + largest);
+        System.out.println("The smalles integer is: " + smallest);
+        input.close();
+    }
+}
