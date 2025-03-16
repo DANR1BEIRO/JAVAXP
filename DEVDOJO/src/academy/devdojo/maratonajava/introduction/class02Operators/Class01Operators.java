a7df8f25707ba74ddf6efac753710989c4654fc6
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-16 19:16:30 -0300

    Operators

--- DEVDOJO/src/academy/devdojo/maratonajava/introduction/class02Operators/Class01Operators.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/introduction/class02Operators/Class01Operators.java
@@ -1,6 +1,6 @@
-package academy.devdojo.maratonajava.introducao;
+package academy.devdojo.maratonajava.introduction.class02Operators;
 
-public class Class04Operators {
+public class Class01Operators {
     public static void main(String[] args) {
         // + - * /
         int number1 = 50;
