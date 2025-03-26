ad62b5d77fa37d1cd25f2112985d6d9aaf0c4dc7
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-26 19:48:48 -0300

    while and for loops test

--- JAVA - Use a cabeca/src/TenGreenBottles.java
+++ JAVA - Use a cabeca/src/TenGreenBottles.java
@@ -1,19 +1,19 @@
 public class TenGreenBottles {
     public static void main(String[] args) {
         int bottles = 10;
-        String x = "bottles";
+        String word = "bottles";
 
         while (bottles > 0) {
             if (bottles == 1) {
-                x = "bottle";
+                word = "bottle";
             }
-            System.out.println("\n" + bottles + " green " + x + " hanging on the wall");
-            System.out.println(bottles + " green " + x + " hanging on the wall");
+            System.out.println("\n" + bottles + " green " + word + " hanging on the wall");
+            System.out.println(bottles + " green " + word + " hanging on the wall");
             System.out.println("And if one green bottle should accidentally fall");
             bottles--;
 
             if (bottles > 0) {
-                System.out.println("There'll be " + bottles + " green " + x + " hanging on the wall");
+                System.out.println("There'll be " + bottles + " green " + word + " hanging on the wall");
             } else {
                 System.out.println("There'll be no green bottles hanging on the wall");
             }
