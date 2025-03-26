e336ace9a6330e23559a46dd4d311057c801cf3c
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-26 00:58:08 -0300

    OOP Introduction to methods

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/test/CalculatorTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain.Calculator;
+
+public class CalculatorTest01 {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        calculator.sumTwoNumbers();
+
+        System.out.println("CalculatorTest01 finished");
+
+        calculator.subtractTwoNumbers();
+    }
+}
