a652dcdb654fcaf69ead4a0495d7743c269de0e6
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-06 10:49:41 -0300

    refactored

--- Programming Logic Exercises/src/ex01/Calculator.java
+++ Programming Logic Exercises/src/ex01/Calculator.java
@@ -5,13 +5,14 @@ public class Calculator {
     public String result(int a, int b, int c) {
 
         int sum = a + b;
+
         System.out.println("The sum of " + a + " and " + b + " is equal to " + sum);
         if (sum > c) {
-            return(sum + " is greater than " + c);
+            return (sum + " is greater than " + c);
         } else if (sum < c) {
-            return(sum + " is smaller than " + c);
+            return (sum + " is smaller than " + c);
         } else {
-            return(sum + " is equal to " + c);
+            return (sum + " is equal to " + c);
         }
     }
 }
--- Programming Logic Exercises/src/ex01/main.java
+++ Programming Logic Exercises/src/ex01/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
-        System.out.println(calculator.result(5,4,100));
+        System.out.println(calculator.result(5, 4, 100));
     }
 }
