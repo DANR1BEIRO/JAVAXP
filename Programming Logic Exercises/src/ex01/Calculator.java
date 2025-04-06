a7f166e7ca79bd1bb6ada0e45bc2bb9de7bc9610
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-06 11:27:28 -0300

    Update Calculator.java with new changes

--- Programming Logic Exercises/src/ex01/Calculator.java
+++ Programming Logic Exercises/src/ex01/Calculator.java
@@ -1,6 +1,18 @@
 package ex01;
 
+/**
+ * A calculator class that performs addition and comparison operations
+ */
+
 public class Calculator {
+
+    /**
+     *
+     * @param a the first number
+     * @param b the second number
+     * @param c the number to compare againts the sum of the first and second ones
+     * @return a string describing the relationship between the sum and c
+     */
 
     public String result(int a, int b, int c) {
 
@@ -16,3 +28,4 @@ public class Calculator {
         }
     }
 }
+
