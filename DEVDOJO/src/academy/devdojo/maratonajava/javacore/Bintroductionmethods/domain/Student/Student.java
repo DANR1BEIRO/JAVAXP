2de91b57d57dd64ce1be592a78aa5fd04cd989dc
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-07 00:38:49 -0300

    Reference this

--- DEVDOJO/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/Student/Student.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/Student/Student.java
@@ -4,4 +4,11 @@ public class Student {
     public String name;
     public int age;
     public char gender;
+
+    public void displayThis() {
+        System.out.println("\nDisplay this:");
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.gender);
+    }
 }
