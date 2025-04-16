eb6ee56970507589f741361b65f39a136bd995f8
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-16 15:30:12 -0300

    Aula 63 - Bloco de inicialização static

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/F_StaticModifier/domain/Anime.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.javacore.F_StaticModifier.domain;
+
+public class Anime {
+    private String name;
+    private static int[] episodes;
+
+    /**
+     * 0 - O bloco de inicialização static (assim como todos os atributos static) será executado assim que a JVM carregar a classe
+     * 1 - Alocado espaco em memória para o objeto
+     * 2 - Cada atributo de classe é criado e inicializado com valores default ou o que for passado
+     * 3 - Bloco de inicializacão é executado
+     * 4 - Construtor é executado
+     */
+    static {
+        System.out.println("Dentro do bloco 1 de inicializacão de instância");
+        episodes = new int[100];
+        for (int i = 0; i < episodes.length ; i++) {
+            episodes[i] = i + 1;
+        }
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+        for (int episode : this.episodes) {
+                System.out.print(episode + " ");
+        }
+        System.out.println();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getEpisodes() {
+        return episodes;
+    }
+
+}
---
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/F_StaticModifier/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.F_StaticModifier.test;
+
+import academy.devdojo.maratonajava.javacore.F_StaticModifier.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime();
+        anime.getEpisodes();
+    }
+}
