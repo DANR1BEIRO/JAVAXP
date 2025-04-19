67581e89ab382d13fe614df959f54e7a7cfa567f
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-19 20:42:31 -0300

    Aula 76: Sobrescrita do método toString

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/I_Override/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.I_Override.test;
+
+import academy.devdojo.maratonajava.javacore.I_Override.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Berserk");
+        System.out.println(anime);
+    }
+}
