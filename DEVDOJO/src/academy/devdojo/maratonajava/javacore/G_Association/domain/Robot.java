3cf65902fbf6cb10bc2a3a46d3a74d92d7a04085
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-19 22:07:19 -0300

    Aula 66: Associação pt 03 - Associação unidirecional muitos para um

--- DEVDOJO/src/academy/devdojo/maratonajava/javacore/G_Association/domain/Robot.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/G_Association/domain/Robot.java
@@ -1,34 +1,48 @@
-    package academy.devdojo.maratonajava.javacore.G_Association.domain;
+package academy.devdojo.maratonajava.javacore.G_Association.domain;
 
-    public class Robot {
-        public String name;
-        public Supervisor supervisor;
+public class Robot {
+    public String name;
+    public Supervisor supervisor;
+    public RobotEnergy energy;
 
-        public void info(){
-            System.out.println("______________________________________________________");
-            System.out.println("Robot " + this.name + " is supervisedy by " + this.supervisor.getName());
-        }
-        public Robot(String name, Supervisor supervisor) {
-            this.name = name;
-            this.supervisor = supervisor;
-        }
+    public void info() {
+        System.out.println("______________________________________________________");
+        String energyType = this.energy.getEnergy();
+        String energyPhrase = energyType.equalsIgnoreCase("electricity")
+                ? "electricity"
+                : energyType + " energy";
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Supervisor getSupervisor() {
-            return supervisor;
-        }
-
-        public void setSupervisor(Supervisor supervisor) {
-            this.supervisor = supervisor;
-        }
+        System.out.println("Robot " + this.name + " is powered by " + energyPhrase + "." +
+                "\nSupervisedy by " + this.supervisor.getName());
     }
+
+    public Robot(String name, Supervisor supervisor) {
+        this.name = name;
+        this.supervisor = supervisor;
+    }
+
+    public Robot(String name, Supervisor supervisor, RobotEnergy energy) {
+        this.name = name;
+        this.supervisor = supervisor;
+        this.energy = energy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Supervisor getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Supervisor supervisor) {
+        this.supervisor = supervisor;
+    }
+}
 
 
 
--- DEVDOJO/src/academy/devdojo/maratonajava/javacore/G_Association/test/RobotTest01.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/G_Association/test/RobotTest01.java
@@ -1,16 +1,20 @@
 package academy.devdojo.maratonajava.javacore.G_Association.test;
 
 import academy.devdojo.maratonajava.javacore.G_Association.domain.Robot;
+import academy.devdojo.maratonajava.javacore.G_Association.domain.RobotEnergy;
 import academy.devdojo.maratonajava.javacore.G_Association.domain.Supervisor;
 
 public class RobotTest01 {
     public static void main(String[] args) {
 
+        RobotEnergy solar = new RobotEnergy("solar");
+        RobotEnergy eletric = new RobotEnergy("electricity");
+
         Supervisor supervisor = new Supervisor("Dra. Susan Calvin");
 
-        Robot robot1 = new Robot("Wall-E", supervisor);
-        Robot robot2 = new Robot("Sonny", supervisor);
-        Robot robot3 = new Robot("Optmus Prime", supervisor);
+        Robot robot1 = new Robot("Wall-E", supervisor, solar);
+        Robot robot2 = new Robot("Sonny", supervisor, solar);
+        Robot robot3 = new Robot("Optmus Prime", supervisor, eletric);
 
         robot1.info();
         robot2.info();
