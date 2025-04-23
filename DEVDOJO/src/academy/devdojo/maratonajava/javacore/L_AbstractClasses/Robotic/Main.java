236c6856aa0543dd1fca00b6a2bc4924f87ba6f8
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-23 12:48:49 -0300

    Refatorei a criação dos objetos no main. Modo de criação anterior: Subclasse + variável de referencia = new subclasse. Agora: Super abstrata + variável de referência = new subclasse. Deixa o código bem mais limpo e fácil de ler.

--- DEVDOJO/src/academy/devdojo/maratonajava/javacore/L_AbstractClasses/Robotic/Main.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/L_AbstractClasses/Robotic/Main.java
@@ -3,13 +3,13 @@ package academy.devdojo.maratonajava.javacore.L_AbstractClasses.Robotic;
 public class Main {
     public static void main(String[] args) {
 
-        DeliveryRobot deliveryRobot = new DeliveryRobot("Hermes", "HE-0102", 50, 15);
-        CleaningRobot cleaningRobot = new CleaningRobot("Wall-E", "WE-0001", 50, 10);
-        WelderRobot welderRobot = new WelderRobot("Sonny", "NS-5", 80, 30, "NASA", 90);
-        AssemblerRobot assemblerRobot = new AssemblerRobot("Optmus Prime", "OP-9922", 80,30, "Tesla Brazil");
+        Robot deliveryRobot = new DeliveryRobot("Hermes", "HE-0102", 50, 15);
+        Robot cleaningRobot = new CleaningRobot("Wall-E", "WE-0001", 50, 10);
+        Robot welderRobot = new WelderRobot("Sonny", "NS-5", 80, 30, "NASA", 90);
+        Robot assemblerRobot = new AssemblerRobot("Optmus Prime", "OP-9922", 80, 30, "Tesla Brazil");
 
-        Robot[] robots = {deliveryRobot, cleaningRobot, welderRobot,assemblerRobot};
-        for(Robot robot : robots){
+        Robot[] robots = {deliveryRobot, cleaningRobot, welderRobot, assemblerRobot};
+        for (Robot robot : robots) {
             robot.turnOn();
             robot.performTask();
             robot.rechargeBattery(20);
