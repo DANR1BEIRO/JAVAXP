ceb581c714a15a30414fdab6f9e77d79f5c6c71d
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-27 20:04:59 -0300

    Trocada a referência do objeto Tomato; o objeto fica inacessível e será coletado pelo garbage collector porque ficou sem referência.

--- DEVDOJO/src/academy/devdojo/maratonajava/javacore/N_Polymorphism/test_Controller/ProductTest02.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/N_Polymorphism/test_Controller/ProductTest02.java
@@ -24,5 +24,19 @@ public class ProductTest02 {
         System.out.println(tomato.calculateTaxValue());
         System.out.println(tomato.getBestBefore());
 
+        product2 = new Computer("novo computador",5);
+        System.out.println(product2.getName());
+        System.out.println(product2.getPrice());
+        System.out.println(product2.calculateTaxValue());
+        /**
+         * O objeto Tomato que estava sendo referenciado por product2 ainda existe
+         * na memória imediatamente após a atribuição, mas agora está sem referência
+         * (a menos que alguma outra variável ainda aponte para ele).
+         *
+         * Como não há mais nenhuma variável apontando para aquele objeto, ele se torna
+         * inacessível pelo código. Em Java, objetos sem referência ficam "órfãos" e, em algum
+         * momento, o garbage collector (coletor de lixo) do Java irá liberar a memória ocupada
+         * por esse objeto automaticamente.
+         */
     }
 }
