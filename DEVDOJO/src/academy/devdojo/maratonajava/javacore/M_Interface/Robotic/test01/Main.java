e7b21b75573b58aa40a6b0293c88cdd053e64497
DANR1BEIRO <danribeiro001@gmail.com>
2025-04-27 13:28:17 -0300

    Testing decoupling code

--- DEVDOJO/src/academy/devdojo/maratonajava/javacore/M_Interface/Robotic/test01/Main.java
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/M_Interface/Robotic/test01/Main.java
@@ -3,19 +3,14 @@ package academy.devdojo.maratonajava.javacore.M_Interface.Robotic.test01;
 public class Main {
     public static void main(String[] args) {
 
-        Robot flyingRobot = new FlyingRobot();
-        Robot swimmingRobot = new SwimmingRobot();
-        Robot landRobot = new LandRobot();
-
         Robot[] robots = {
-                flyingRobot,
-                swimmingRobot,
-                landRobot
+                new FlyingRobot(),
+                new LandRobot(),
+                new SwimmingRobot()
         };
 
         for (Robot robot : robots) {
             System.out.println("______________________");
-            System.out.println("Inside the for loop:");
             robot.turnOn();
             robot.move();
             robot.turnOff();
