c2ef2169afacda22577a861aa892d20a98691ccc
DANR1BEIRO <danribeiro001@gmail.com>
2025-05-26 20:29:13 -0300

    Aula 99 - exceções pt 05: Lançando exceções checked

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/O_Exception/exception/test/ExceptionTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.O_Exception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException {
+        createNewFile();
+    }
+
+    private static void createNewFile() throws IOException {
+        File file = new File("testFile\\test01.txt");
+        try {
+            boolean isCreated = file.createNewFile();
+            System.out.println("File created: " + isCreated);
+        } catch (IOException e) {
+            // JAMAIS DEIXE O BLOCK CATCH EM BRANCO
+            // NÃO COLOQUE LÓGICA DE NEGÓCIO NO CATCH
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
