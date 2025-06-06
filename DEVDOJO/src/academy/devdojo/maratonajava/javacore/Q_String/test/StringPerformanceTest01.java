2c07cf5f4df2c98e26066dd35483d4281c51aeed
Daniel Ribeiro <danribeiro001@gmail.com>
2025-06-06 15:34:47 -0300

    Aula  110 - Utility Classes - String pt 03: Performance

---
+++ DEVDOJO/src/academy/devdojo/maratonajava/javacore/Q_String/test/StringPerformanceTest01.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.Q_String.test;
+
+public class StringPerformanceTest01 {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        concatString(100_000);
+        long end = System.currentTimeMillis();
+        System.out.println("Time normal string: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        end = System.currentTimeMillis();
+        System.out.println("Time string builder: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        end = System.currentTimeMillis();
+        System.out.println("Time string buffer: " + (end - start) + "ms");
+
+    }
+
+    private static void concatString(int length) {
+        String text = "";
+        for (int i = 0; i < length; i++) {
+            text += i;
+        }
+    }
+
+    public static void concatStringBuilder(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(i);
+        }
+    }
+
+    public static void concatStringBuffer(int length) {
+        StringBuffer sb = new StringBuffer(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(i);
+        }
+    }
+}
